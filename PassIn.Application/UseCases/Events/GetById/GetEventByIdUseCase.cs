c53d94a8455dea3078da538380c473db467701bb
Lazaro Mbaila <lazarozito@gmail.com>
2024-08-04 19:50:20 +0200

    Add project files.

---
+++ PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
@@ -0,0 +1,33 @@
+﻿using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PassIn.Application.UseCases.Events.GetById
+{
+    public class GetEventByIdUseCase
+    {
+        public ResponseEventJson Execute(Guid id)
+        {
+            var dbContext = new PassInDbContext();
+            var entity = dbContext.Events.Find(id);
+            if (entity is null)
+                throw new PassInException("An event with this is dont exist");
+
+            return new ResponseEventJson
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                Details = entity.Details,
+                MaximumAttendees = entity.Maximum_Attendees,
+                AttendeesAmount = -1
+            };
+
+        }
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,57 @@
+﻿using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+        public ResponseRegisteredEventJson Execute(RequestEventJson request)
+        {
+            Validate(request);
+
+            var dbContext = new PassInDbContext();
+            var entity = new Infrastructure.Entities.Event
+            { 
+                Title = request.Title,
+                Details = request.Details,
+                Maximum_Attendees = request.MaximumAttendees,
+                Slug = request.Title.ToLower().Replace(" ", "-"),
+   
+            };
+
+            dbContext.Events.Add(entity);
+            dbContext.SaveChanges();
+
+            return new ResponseRegisteredEventJson
+            {
+                Id = entity.Id
+            };
+
+        }
+
+        private void Validate(RequestEventJson request)
+        {
+            if(request.MaximumAttendees <= 0)
+            {
+                throw new PassInException("The Maximum attendes is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Title)) {
+                throw new PassInException("The title  is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Details))
+            {
+                throw new PassInException("The detail is invalid.");
+            }
+        }
+
+    }
+}
